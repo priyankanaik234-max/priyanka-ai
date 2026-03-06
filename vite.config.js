fae1176d33627bbd7c00692bef6713d6a14595e5
priyankanaik234-max <priyankanaik234@gmail.com>
2026-03-06 16:46:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-{
-  "buildCommand": "npm install && npx vite build",
-  "outputDirectory": "dist",
-  "framework": null
-}
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
